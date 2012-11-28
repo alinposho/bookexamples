45ef90673e2f72b2cd8deeb1479801fc74389437
aposorovaschi <aposorovaschi@tim-lap-144>
2012-11-28 17:43:18 +0200

    Change erroneous character in comments.

--- src/chapter7/InfiniteWhileLoop.scala
+++ src/chapter7/InfiniteWhileLoop.scala
@@ -5,7 +5,7 @@ object InfiniteWhileLoop extends App{
   println("Write somthing to inlustrate the usage of the infinite while loop:")
 
   var line = ""
-  while ((line = readLine()) != "") // This doesn’t work!
+  while ((line = readLine()) != "") // This does not work!
     println("Read: " + line)
   
 }
