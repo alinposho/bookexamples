4c14586d695b2fd5c7928fbcf003d5f08eb4eb24
aposorovaschi <aposorovaschi@tim-lap-144>
2012-11-28 17:59:43 +0200

    Refactor the local function.

--- src/chapter8/LongLinesLocalFunction.scala
+++ src/chapter8/LongLinesLocalFunction.scala
@@ -6,7 +6,7 @@ object LongLinesLocalFunction {
 
   def processFile(filename: String, width: Int) {
 
-    def processLine(filename: String, line: String, width: Int) {
+    def processLine(line: String) {
       if (line.length() > width) {
         println(filename + ": " + line)
       }
@@ -14,7 +14,7 @@ object LongLinesLocalFunction {
 
     val source = Source.fromFile(filename)
     for (line <- source.getLines)
-      processLine(filename, line, width)
+      processLine(line)
   }
 
 }
