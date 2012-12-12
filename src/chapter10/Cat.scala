4309b71271251c144e4b3dffad61483cd9cd038a
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2012-12-12 14:28:22 +0200

    An interesting use of access modifiers in a class' constructor.

---
+++ src/chapter10/Cat.scala
@@ -0,0 +1,11 @@
+package chapter10
+
+class Cat {
+  val dangerous = false
+}
+
+// Interesting enough one can define a private field in the class constructor
+class Tiger(
+  override val dangerous: Boolean,
+  private var age: Int,
+  private val clawLength: Int) extends Cat
