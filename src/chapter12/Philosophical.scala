eaa2f9a4a8759c21785279e1b55285eb182d6e6f
Alin Posorovaschi <alinposho@yahoo.com>
2012-12-14 06:44:05 +0200

    Chapter 10 - Trait usage example.

--- src/chapter12/Philosophical.scala
+++ src/chapter12/Philosophical.scala
@@ -1,7 +1,11 @@
 package chapter12
 
 trait Philosophical {
-	def philosophize() {
-	  println("I consume memory therefore, I am!")
-	}
+  def philosophize() {
+    println("I consume memory therefore, I am!")
+  }
+}
+
+class Frog extends Philosophical {
+  override def toString = "green"
 }
---
+++ src/chapter12/simpletraits.sc
@@ -0,0 +1,10 @@
+package chapter12
+
+object simpletraits {
+
+	val frog = new Frog                       //> frog  : chapter12.Frog = green
+	frog.philosophize                         //> I consume memory therefore, I am!
+
+
+
+}
