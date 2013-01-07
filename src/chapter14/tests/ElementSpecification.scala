0837f7273ceb633b35bc31db97fce00719a8bda5
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-01-07 12:59:02 +0200

    Before removing the specs2 example from the project because of compilation errors/

--- src/chapter14/tests/ElementSpecification.scala
+++ src/chapter14/tests/ElementSpecification.scala
@@ -2,7 +2,17 @@ package chapter14.tests
 
 import chapter10.puttingitalltogether.Element.elem
 import org.specs2.Specification
+import org.specs2.matcher.ShouldMatchers
+import org.scalatest.matchers.ShouldMatchers
+
 
 object ElementSpecification extends Specification {
+  
+  "A UnitoformElement" should {
+    "have a width equal to the passed value" in {
+      val ele = elem('x', 2, 3)
+      ele.width must be_==(2)
+    }
+  }
   
 }
