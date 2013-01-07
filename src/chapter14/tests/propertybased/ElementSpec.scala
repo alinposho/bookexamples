74be293bbea1f3e41e08be455620b52e4b52c0c4
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-01-07 13:21:38 +0200

    Add an example of scalacheck that compiles but doesn't run.

---
+++ src/chapter14/tests/propertybased/ElementSpec.scala
@@ -0,0 +1,21 @@
+package chapter14.tests.propertybased
+
+import chapter10.puttingitalltogether.Element.elem
+import org.scalatest.prop.Checkers
+import org.scalacheck.Prop._
+import org.junit.runner.RunWith
+import org.scalatest.WordSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ElementSpec extends WordSpec with Checkers {
+
+  "elem result" must {
+    "have passed width" in {
+      check((w: Int) => w > 0 ==> (elem('x', w, 3).width == w))
+    }
+    "have passed height" in {
+      check((h: Int) => h > 0 ==> (elem('x', 2, h).height == h))
+    }
+  }
+}
