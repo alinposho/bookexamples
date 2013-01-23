5a9d59c5757869bd21618fc740d0d8d7a118d19c
aposorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-01-23 14:55:02 +0200

    A more eloquent example of how to send/receive messages to/from a Scala actor.

---
+++ src/chapter32/akka/NameResolver.scala
@@ -0,0 +1,42 @@
+package chapter32.akka
+
+import scala.actors._
+import scala.actors.Actor._
+
+object NameResolver extends Actor {
+
+  import java.net.{ InetAddress, UnknownHostException }
+
+  def act() {
+    react {
+      case (name: String, actor: Actor) =>
+        actor ! getIp(name)
+        act()
+      case "EXIT" =>
+        println("Name resolver exiting.")
+      // quit
+      case msg =>
+        println("Unhandled message: " + msg)
+        act()
+    }
+  }
+
+  def getIp(name: String): Option[InetAddress] = {
+    try {
+      Some(InetAddress.getByName(name))
+    } catch {
+      case _: UnknownHostException => None
+    }
+  }
+
+  def main(args: Array[String]) {
+    NameResolver.start()
+
+    NameResolver ! ("www.scala-lang.org", self)
+
+    self.receiveWithin(1000) { case x => println("received: " + x) }
+
+    NameResolver ! ("EXIT")
+  }
+
+}
