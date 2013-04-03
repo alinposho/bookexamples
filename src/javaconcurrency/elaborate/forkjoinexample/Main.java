a60e8f4e5008b3b12beef4d61493c79ec6e10e18
alinposho <alinposho@yahoo,com>
2013-04-03 06:43:17 +0300

    Refactor ForkJoin Java example to make it easier to understand.

--- src/javaconcurrency/elaborate/forkjoinexample/Main.java
+++ src/javaconcurrency/elaborate/forkjoinexample/Main.java
@@ -8,14 +8,16 @@ public class Main {
 		WordCounter wordCounter = new WordCounter();
 		Folder folder = Folder.fromDirectory(new File(args[0]));
 		long time = System.currentTimeMillis();
-		System.out.println(wordCounter.countOccurrencesOnSingleThread(folder,
-				args[1]));
+		System.out.println("The number of occurences of the word " + args[1]
+				+ "\n in the files from " + args[0] + " folder\n is "
+				+ wordCounter.countOccurrencesOnSingleThread(folder, args[1]));
 		long newTime = System.currentTimeMillis();
 		System.out.println("Execution of the single threaded code took: "
 				+ (newTime - time));
 
-		System.out.println(wordCounter.countOccurrencesInParallel(folder,
-				args[1]));
+		System.out.println("The number of occurences of the word " + args[1]
+				+ "\n in the files from " + args[0] + " folder \n is "
+				+ wordCounter.countOccurrencesInParallel(folder, args[1]));
 
 		System.out.println("Execution of the multithreaded code took: "
 				+ (System.currentTimeMillis() - newTime));
