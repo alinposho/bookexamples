21c6c945105a3ec26d0d65eb6a9c9fcd27df6d8f
alinposho <alinposho@yahoo,com>
2013-04-26 06:00:16 +0300

    Add a sample code for a list of method implicit parameters.

--- src/programming/in/scala/chapter21/implicits/ImplicitParameters.scala
+++ src/programming/in/scala/chapter21/implicits/ImplicitParameters.scala
@@ -1,6 +1,7 @@
 package programming.in.scala.chapter21.implicits
 
 class PreferredPrompt(val preferences: String)
+class PreferredDrink(val preference: String)
 
 object Greeter {
   def greet(name: String)(implicit prompt: PreferredPrompt) {
@@ -9,20 +10,41 @@ object Greeter {
   }
 }
 
+object Greeter2ListOfImplicits {
+  /**
+   * Notice the list of implicit parameters in the method signature
+   */
+  def greet(name: String)(implicit prompt: PreferredPrompt, drink: PreferredDrink) {
+    println(" Welcome, " + name + ". The system is ready.");
+    println("But while you work, ")
+    println("Why not enjoy a cup of " + drink.preference + "?")
+    println(prompt.preferences)
+  }
+}
+
 object JoesPrefs {
   implicit val prompt = new PreferredPrompt("Yes, master>")
+  // Notice that the name of the implicit parameter doesn't have to match the 
+  // method's argument name
+  implicit val somethigToDring = new PreferredDrink("tea")
 }
 
 object ImplicitParameters {
 
   def main(args: Array[String]) {
-    
-//    Greeter.greet("Helen") // Won't compile since the "prompt" implicit parameter is not defined 
+
+    //    Greeter.greet("Helen") // Won't compile since the "prompt" implicit parameter is not defined 
     Greeter.greet("Helen")(new PreferredPrompt("relax>"))
-    
+
     // Notice that we can import something in the middle of the function.
     import JoesPrefs._
     // Without the above import we would have to call greet providing the value for the implicit parameter
     Greeter.greet("Joe")
+
+    // One can invoke the method with implicit parameters explicitly
+    Greeter2ListOfImplicits.greet("Helen")(new PreferredPrompt("relax>"), new PreferredDrink("Cola"))
+    // Onr implicitly
+    Greeter2ListOfImplicits.greet("Joe")
+
   }
 }
