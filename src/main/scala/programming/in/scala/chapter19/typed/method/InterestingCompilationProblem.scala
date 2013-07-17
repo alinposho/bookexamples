cdd12a7920e87202989a34353bfd6bc070724f67
Alin Posorovaschi <alin.posorovaschi@3pillarglobal.com>
2013-07-17 10:08:53 +0300

    Add an workaround for the inherited generic method.

--- src/main/scala/programming/in/scala/chapter19/typed/method/InterestingCompilationProblem.scala
+++ src/main/scala/programming/in/scala/chapter19/typed/method/InterestingCompilationProblem.scala
@@ -1,10 +1,37 @@
 package programming.in.scala.chapter19.typed.method
 
+import scala.collection.immutable.LinearSeq
+
 trait Super {
-  def computeSmth[T <: Traversable[Int]](): List[T]  
+  def computeSmth[T <: AnyVal](): List[T]
 }
 
 // The following class will not compile - Evidently, I need to learn more about typed parameters 
 //class Implementation extends Super {
-//  def computeSmth[List[Int]](): List[List[Int]] = List(List(1, 2, 3))
+//  var list = List(1, 2, 3)
+//  def computeSmth[Int](): List[Int] = list 
 //}
+
+// But this will work...
+trait SuperWithType {
+  type U <: LinearSeq[Int]
+  def computeSmthElse(): List[U]
+}
+
+class ImplementationUsingType extends SuperWithType {
+  type U = List[Int]
+  def computeSmthElse(): List[List[Int]] = List(List(1, 2, 3))
+}
+
+
+// But this will work...
+trait SuperWithTypeSimplified {
+  type U <: AnyVal
+  def computeSmthElse(): List[U]
+}
+
+class ImplementationUsingTypeSimplified extends SuperWithTypeSimplified {
+  type U = Int
+  def computeSmthElse(): List[Int] = List(1, 2, 3)
+}
+
