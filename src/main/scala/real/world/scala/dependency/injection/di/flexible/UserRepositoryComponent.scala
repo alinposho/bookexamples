b0d3369783411263fa0f899805ca4d103e65b679
Alin Posorovaschi <alin.posorovaschi@workday.com>
2013-10-24 16:01:03 +0100

    Remove unnecessary spaces form the Cake Pattern sample code.

--- src/main/scala/real/world/scala/dependency/injection/di/flexible/UserRepositoryComponent.scala
+++ src/main/scala/real/world/scala/dependency/injection/di/flexible/UserRepositoryComponent.scala
@@ -3,6 +3,5 @@ package real.world.scala.dependency.injection.di.flexible
 import real.world.scala.dependency.injection.no.di.UserRepository
 
 trait UserRepositoryComponent {
-
   val userRepository: UserRepository
 }
--- src/main/scala/real/world/scala/dependency/injection/di/flexible/UserServiceComponent.scala
+++ src/main/scala/real/world/scala/dependency/injection/di/flexible/UserServiceComponent.scala
@@ -3,7 +3,5 @@ package real.world.scala.dependency.injection.di.flexible
 import real.world.scala.dependency.injection.no.di.UserService
 
 trait UserServiceComponent {
-
   val userService: UserService
-
 }
