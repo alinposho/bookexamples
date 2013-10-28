cdb75611aad8c4e9f09d9a8bc83f738c312e3e22
alinposho <alinposho@yahoo.com>
2013-10-28 21:17:53 +0000

    Rename classes to avoid name collisions within the package

--- src/main/scala/scalapuzzlers/ATroubleWithTraits2.scala
+++ src/main/scala/scalapuzzlers/ATroubleWithTraits2.scala
@@ -1,23 +1,23 @@
 package scalapuzzlers
 
-trait AA {
+trait A2 {
   val foo: Int
   val bar = 10
   println("In A: foo: " + foo + ", bar: " + bar)
 }
 
-class BB extends AA {
+class BB2 extends A2 {
   val foo: Int = 25
   println("In B: foo: " + foo + ", bar: " + bar)
 }
 
-class CC extends BB {
+class C2 extends BB2 {
   override val bar = 99
   println("In C: foo: " + foo + ", bar: " + bar)
 }
 
 object ATroubleWithTraits2 {
   def main(args: Array[String]): Unit = {
-    new CC
+    new C2
   }
 }
