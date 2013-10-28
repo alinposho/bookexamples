0a610ec5febebac3019d6e17fb20fbbecdd33e3f
alinposho <alinposho@yahoo.com>
2013-10-28 14:55:48 +0000

    An example of how generic parameters and implicits mix together

---
+++ src/main/scala/scalapuzzlers/ArghArrgh.scala
@@ -0,0 +1,18 @@
+package scalapuzzlers
+
+object ArghArrgh {
+  def main(args: Array[String]): Unit = {
+    def square[T: Numeric](n: T) = implicitly[Numeric[T]].times(n, n)
+
+    def twiceA[T](f: T => T, a: T) = f(f(a))
+    def twiceB[T](f: T => T)(a: T) = f(f(a))
+    def twiceC[T](a: T, f: T => T) = f(f(a))
+    def twiceD[T](a: T)(f: T => T) = f(f(a))
+
+    // The first three will not compile since the type for the square function is not known at compile type
+//    twiceA(square, 2)
+//    twiceB(square)(2)
+//    twiceC(2, square)
+    twiceD(2)(square)
+  }
+}
