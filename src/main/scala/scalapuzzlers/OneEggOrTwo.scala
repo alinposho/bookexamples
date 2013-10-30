b461ae09c1110e037db83aac50ad606862814ce6
Alin Posorovaschi <alin.posorovaschi@workday.com>
2013-10-30 16:48:18 +0000

    Update puzzler with explanations.

--- src/main/scala/scalapuzzlers/OneEggOrTwo.scala
+++ src/main/scala/scalapuzzlers/OneEggOrTwo.scala
@@ -7,8 +7,12 @@ object OneEggOrTwo {
 
   class C
   def main(args: Array[String]) {
-    val x1, x2 = new C
-    val y1 @ y2 = new C
+    val x1, x2 = new C // This gets translated into two different initializations
+    val y1 @ y2 = new C // This gets translated into
+    
+    val t = new C match {
+      case y11 @ y12 => (y11, y12)
+    }
     
     println(x1 == x2)
     println(y1 == y2)
