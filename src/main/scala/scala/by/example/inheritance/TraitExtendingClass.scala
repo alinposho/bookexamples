41a7f7442305a746697293c0dc6d7ba93231accd
alinposho <alinposho@yahoo.com>
2013-11-04 06:39:26 +0000

    Execute the class extending trait extending class code.

--- src/main/scala/scala/by/example/inheritance/TraitExtendingClass.scala
+++ src/main/scala/scala/by/example/inheritance/TraitExtendingClass.scala
@@ -1,13 +1,27 @@
 package scala.by.example.inheritance
 
 object TraitExtendingClass extends App {
+	SubObject.smth
 	
+	new SubClass("Called from main").smth
+}
+
+// object SubObject extends SubClass { // This will not work, of course, since name is not defined
+object SubObject extends SubClass("SubObject") {
+  override def smth: Unit = {
+    println("SubObject")
+  }
 }
 
 // class SubClass extends SubTrait { }// Will ask for SuperType contructor parameters
-//class SubClass(val name: String) extends SubTrait { }// This will not work either 
+//class SubClass(val name: String) extends SubTrait { }// This will not work ei
+class SubClass(name: String) extends SuperClass(name) with SubTrait {
+  override def smth: Unit = {
+    println("SubClass")
+  }
+}
 
-trait SubTrait extends SuperClass{
+trait SubTrait extends SuperClass {
   override def smth: Unit = {
     println("SubTrait")
   }
