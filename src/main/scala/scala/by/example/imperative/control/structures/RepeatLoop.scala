139afff5ca17a8e654dcc544ecbd1b8590aacc22
Alin Posorovaschi <alin.posorovaschi@workday.com>
2013-11-14 13:45:10 +0000

    Add a more elegant repeat implementation suggested by Fergal

--- src/main/scala/scala/by/example/imperative/control/structures/RepeatLoop.scala
+++ src/main/scala/scala/by/example/imperative/control/structures/RepeatLoop.scala
@@ -1,24 +1,42 @@
 package scala.by.example.imperative.control.structures
 
 object RepeatLoop {
-  
+
   class EC(command: => Unit) {
-    def until(cond: => Boolean):Unit = {
-      if(cond) ()
+    def until(cond: => Boolean): Unit = {
+      if (cond) ()
       else {
         command
         until(cond)
       }
     }
   }
-  
+
+  def repeat(command: => Unit) = new { // This is equivalent to new AnyRef{def until ...}
+    def until(cond: => Boolean): Unit = {
+      if (cond) ()
+      else {
+        command
+        until(cond)
+      }
+    }
+  }
+
   def repeatLoop(command: => Unit) = new EC(command)
-  
-  def main(args: Array[String]):Unit = {
-    
-    var i = 0;
-    
-    repeatLoop{
+
+  def main(args: Array[String]): Unit = {
+
+    var i = 0
+
+    println("The output of repeatLoop")
+    repeatLoop {
+      println(s"i = ${i}")
+      i += 1
+    } until (i == 10)
+
+    println("The output of repeat")
+    i = 0
+    repeat {
       println(s"i = ${i}")
       i += 1
     } until (i == 10)
