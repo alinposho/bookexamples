95444b46d5bccf1939d1313eb44d07c93d6e757d
Alin Posorovaschi <alin.posorovaschi@workday.com>
2014-01-21 11:12:37 +0000

    Program that prints itself works perfectly

--- src/main/scala/reflections/on/trusting/trust/PrintItself.scala
+++ src/main/scala/reflections/on/trusting/trust/PrintItself.scala
@@ -1,6 +1,13 @@
 package reflections.on.trusting.trust
 
 object PrintItself extends App {
-  val s = "package reflections.on.trusting.trust  object PrintItself extends App {  val s = \"%s\"  print(s, s)  }"
-  printf(s, s)
+  val s = """package reflections.on.trusting.trust
+
+object PrintItself extends App {
+  val s = ""\"%s""\"
+  printf(s.replace("\\", ""), s)
 }
+    """
+  printf(s.replace("", ""), s)
+}
+    
